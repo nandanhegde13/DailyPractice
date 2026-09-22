005a17cfb3241923fa7388cb71aab38c39ea628a
Nandan <hegdenandan013@gmail.com>
2026-09-22 11:29:01 +0530

    22-09-2026

---
+++ src/test/java/Test1.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+	//Alerts in selenium
+	@Test
+	public void testMethod1() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("alertbtn")).click();
+		Thread.sleep(3000);
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("confirmbtn")).click();
+		Thread.sleep(3000);
+		driver.switchTo().alert().dismiss();
+	}
+
+}
