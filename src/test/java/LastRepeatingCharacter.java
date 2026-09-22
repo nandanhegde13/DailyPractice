27294ec3e78a10ac390497045376305d5597aa55
Nandan <hegdenandan013@gmail.com>
2026-09-22 22:22:53 +0530

    test

---
+++ src/test/java/LastRepeatingCharacter.java
@@ -0,0 +1,41 @@
+
+public class LastRepeatingCharacter {
+
+	public static void main(String args[])
+	{
+		String name = "nanda";
+		char repeatingChar=' ';
+		
+		for(int i=name.length()-1;i>=0;i--)
+		{
+			boolean repeating = false;
+			
+			char ch = name.charAt(i);
+			
+			for(int j=0;j<=name.length()-1;j++)
+			{
+				if(j!=i && ch==name.charAt(j))
+				{
+					repeating =true;
+					break;
+				}
+			}
+				
+				if(repeating==true)
+				{
+					repeatingChar=ch;
+					break;
+				}		
+			
+			
+		}
+		
+		if(repeatingChar!=' ')
+		{
+			System.out.println(repeatingChar);
+		}
+		
+		
+	}
+	
+}
