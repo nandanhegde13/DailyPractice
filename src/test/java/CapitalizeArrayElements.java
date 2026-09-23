8249b7d51219cf6305287f4d90e54a809bdc5598
Nandan <hegdenandan013@gmail.com>
2026-09-23 07:11:36 +0530

    23-09-2026

---
+++ src/test/java/CapitalizeArrayElements.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class CapitalizeArrayElements {
+
+	public static void main(String args[])
+	{
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String>arr1= new ArrayList<String>();
+		arr.add("Apple");
+		arr.add("Ban");
+		arr.add("Ora");
+		arr.add("Grape");
+		arr.forEach(item->{if(item.length()==3)arr1.add(item.toUpperCase());});
+        
+		arr1.forEach(item->System.out.println(item));
+		
+	}
+}
