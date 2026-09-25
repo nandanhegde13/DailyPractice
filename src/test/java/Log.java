35eb9a29711c8d31806657357bdaf2ec2d33d0d3
Nandan <hegdenandan013@gmail.com>
2026-09-25 20:23:35 +0530

    '25-09-2026'

---
+++ src/test/java/Log.java
@@ -0,0 +1,22 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+
+	private static Logger logger = LogManager.getLogger(Log.class);
+	
+	public static void info(String message)
+	{
+		logger.info(message);
+	}
+	
+	public static void debug(String message)
+	{
+		logger.debug(message);
+	}
+	
+	public static void error(String message)
+	{
+		logger.error(message);
+	}
+}
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -11,7 +11,9 @@ public class Test1 {
 	public void testMethod1() throws InterruptedException
 	{
 		WebDriver driver = new ChromeDriver();
+		Log.info("Getting url");
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		Log.info("Clicking on alert button");
 		driver.findElement(By.id("alertbtn")).click();
 		Thread.sleep(3000);
 		driver.switchTo().alert().accept();
