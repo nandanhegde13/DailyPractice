96f036819488b748074c1019e73097379ac59b69
Nandan <hegdenandan013@gmail.com>
2026-09-25 21:13:01 +0530

    '25-09-2026'

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,16 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-public class Test1 {
+@Listeners(Test1.class)
+public class Test1  implements ITestListener{
 	
+	static WebDriver driver;
 	
 	//Alerts in selenium
+	
 	@Test
-	public void testMethod1() throws InterruptedException
+	public void testMethod1() throws InterruptedException 
 	{
-		WebDriver driver = new ChromeDriver();
+		this.driver = new ChromeDriver();
 		Log.info("Getting url");
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		Log.info("Clicking on alert button");
@@ -20,7 +33,52 @@ public class Test1 {
 		driver.findElement(By.id("confirmbtn")).click();
 		Thread.sleep(3000);
 		driver.switchTo().alert().dismiss();
-		driver.quit();
+		
+		
+		
+	}
+	
+	private static String takeScreenshot() throws WebDriverException, IOException
+	{
+        TakesScreenshot ts = (TakesScreenshot)driver;
+        String filePath=System.getProperty("user.dir")
+                + "/pictures/"      
+                + "_Thread-"
+                + Thread.currentThread().getId()
+                + "_"
+                + System.currentTimeMillis()
+                + ".png";
+		
+		FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File(filePath));
+		return filePath;
+	}
+	
+	@Override
+	public  void onTestSuccess(ITestResult result)
+	{
+		try {
+			Test1.takeScreenshot();
+		} catch (WebDriverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public  void onTestFailure(ITestResult result)
+	{
+		try {
+			Test1.takeScreenshot();
+		} catch (WebDriverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
